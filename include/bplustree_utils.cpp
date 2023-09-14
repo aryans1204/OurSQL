4ea04c7e7e9f8f84e9165ea3d6ce21bf46889939
Veeraraghavan S Nithyasri <97502384+Veeraraghavan-S-Nithyasri@users.noreply.github.com>
2023-09-14 17:02:01 +0800

    Utility Functions for B+ Tree
    Insertion and Deletion

---
+++ include/bplustree_utils.cpp
@@ -0,0 +1,12 @@
+// We consider the 2 Utility functions: (i) Insertion and (ii) Deletion for our B+ Tree below
+
+// (ii) Deletion
+
+/* In deletion, we need to consider 3 scenarios depending on whether the type of node to be deleted is: leaf, internal, or both
+
+
+*/
+
+
+
+
