32f48a19059ef8b044769fc804dafc92ef6accb5
VSN <97502384+Veeraraghavan-S-Nithyasri@users.noreply.github.com>
2023-09-15 20:01:49 +0800

    bplustree_utils.cpp

--- include/bplustree_utils.cpp
+++ include/bplustree_utils.cpp
@@ -4,8 +4,20 @@
 
 /* In deletion, we need to consider 3 scenarios depending on whether the type of node to be deleted is: leaf, internal, or both
 
+Scenario 1: Leaf node
+            Sub-scenario 1: deletion procedure doesn't cause disturbance to min no of keys => just delete
+            Sub-scenario 2: deletion procesure causes disturbance to min no of keys => delete and get key from neighbor
 
+Scenario 2: Internal node
+            Sub-scenario 1: Delete and get key from left full (meaning having keys > min) child
+            Sub-scenario 2: Delere and get key from right full child
+            Sub-scenario 3: Delete and merge children if both have same no of min keys
+
+Scenario 3: Upon deletion of an internal node, min no of keys not satisfied => merge the neighbor
 */
+
+# include <iostream>
+using namespace std;
 
 
 
