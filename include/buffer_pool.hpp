67fd5ecd0e0a2ad17213d1721693e8d908e2307c
WZHYX <zywongxavier@gmail.com>
2023-09-15 20:51:39 +0800

    bufPool

--- include/buffer_pool.hpp
+++ include/buffer_pool.hpp
@@ -1,5 +1,47 @@
 #include <vector>
+<<<<<<< HEAD
 
+using namespace std;
+=======
+# include "definitions.hpp"
 using namespace BufferPool;
+>>>>>>> 79748290bd2346e66f24694275cab9c7b9e0f498
 
+typedef unsigned int uint;
+typedef unsigned char uchar;
 
+class BufPool{
+private:
+    uchar *memPoolPtr;
+    uchar *blkPtr;
+
+    uint bufPoolSize;
+    uint blkSize;
+    uint bufPoolUsedBlks;
+    uint bufPoolUsedRecords;
+    uint curBlkUsed;
+
+    int numAllocBlks;
+    int numAvailBlks;
+
+public:
+    BufPool(uint bufPoolSize, uint blkSize);
+
+    ~BufPool();
+
+    uint getBlkSize(){
+        return blkSize;
+    }
+
+    uint getBufPoolSize();
+
+    int getMemPoolUsedBlks();
+
+    int getMemPoolUsedRecords();
+
+    int getCurBlkUsed();
+
+    int getNumAllocBlks();
+
+    int getNumAvailBlks();
+};
